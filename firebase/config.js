8080672f499466548b263ce77c1c5ba8086853af
Lucas Alano <lucasdeoliveiraalano@gmail.com>
2023-07-26 13:20:04 -0300

    Initial commit

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6sbmTZ0k6GYAtlTsCMADUVcAty3V4qtc",
+  authDomain: "miniblog-d315d.firebaseapp.com",
+  projectId: "miniblog-d315d",
+  storageBucket: "miniblog-d315d.appspot.com",
+  messagingSenderId: "445627953229",
+  appId: "1:445627953229:web:8b9bd2e429fe11f874ec04"
+};
+
+const app = initializeApp(firebaseConfig);
