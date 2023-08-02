8099be08081297fbeeb21ec081248386956b871a
Lucas Alano <lucasdeoliveiraalano@gmail.com>
2023-08-02 10:23:59 -0300

    Commit Post page
    Commit com a pagina do post, sem css ou funcionalidade, apenas o acesso da mesma pelo id

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import Login from './pages/Login/Login';
 import CreatePost from './pages/CreatePost/CreatePost'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Search from './pages/Search/Search';
+import Post from './pages/Post.js/Post';
 
 //Components
 import Navbar from './components/Navbar';
@@ -53,6 +54,7 @@ function App() {
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
               <Route path='/search' element={<Search />} />
+              <Route path='/posts/:id' element={<Post />} />
               <Route path='/login' element={!user ? <Login /> : <Navigate to='/'/>} />
               <Route path='/register' element={!user ? <Register /> : <Navigate to='/'/>} />
               <Route path='/post/create' element={user ? <CreatePost /> : <Navigate to='/Login'/>}/>
