3425b635f155d7033aecabccabe78dfaaad40c65
Lucas Alano <lucasdeoliveiraalano@gmail.com>
2023-08-02 23:49:26 -0300

    Commit dashboard
    Commit do dashboard, com a busca por uid e criacao do indice no firebase

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -25,10 +25,11 @@ export const useFetchDocuments = (docCollection, search = null, uid= null) => {
                 let q 
 
                 //busca
-                //dasboard
 
                 if (search) {
                     q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"))
+                } else if(uid) {
+                    q = await query(collectionRef, where("uid", "==", uid), orderBy("createdAt", "desc"))
                 } else {
                     q = await query(collectionRef, orderBy('createdAt', 'desc'))
                 }
@@ -55,7 +56,7 @@ export const useFetchDocuments = (docCollection, search = null, uid= null) => {
         }
 
         loadData()
-    }, [docCollection, documents, search, uid, cancelled])
+    }, [docCollection, search, uid, cancelled])
 
     useEffect(() => {
         return () => setCancelled(true)
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,35 @@
 import styles from './Dashboard.module.css'
 
+import {Link} from 'react-router-dom'
+
+//Hooks
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+  
+  //Posts dos usuários
+  const {documents: posts, loading} = useFetchDocuments('posts', null, uid)
+
   return (
     <div>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Gerencie os seus posts</p>
+        {posts && posts.length === 0 ? (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to='/post/create' className='btn'>Criar primeiro post</Link>
+          </div>
+
+        ) : ( 
+        <div>
+
+        </div>)}
+        
+        {posts && posts.map((post) => 
+          <h3>{post.title}</h3>)}
     </div>
   )
 }
